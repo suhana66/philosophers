321f0a1268c653fd28eeec1b61b98e963f5c7f62
Suhana Sajid <susajid@lab3r4s10.42abudhabi.ae>
2024-01-22 12:03:51 +0400

    completed initial create_philos function

--- philo/main.c
+++ philo/main.c
@@ -5,12 +5,88 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/18 17:27:33 by susajid           #+#    #+#             */
-/*   Updated: 2024/01/18 17:34:48 by susajid          ###   ########.fr       */
+/*   Created: 2024/01/24 10:24:33 by susajid           #+#    #+#             */
+/*   Updated: 2024/01/24 17:01:09 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
+static int	parse_unsigned(char *str, unsigned int *result);
+t_philo		*create_philos(t_simulation	sim);
+
 int	main(int argc, char **argv)
 {
+	t_simulation	sim;
+
+	if (argc != 5 && argc != 6)
+		return (ft_perror(USAGE_ERR), 1);
+	sim.quit = false;
+	sim.number_of_meals = -1;
+	if (parse_unsigned(argv[1], &sim.number_of_philo)
+		|| sim.number_of_philo == 0
+		|| parse_unsigned(argv[2], &sim.time_to_die)
+		|| parse_unsigned(argv[3], &sim.time_to_eat)
+		|| parse_unsigned(argv[4], &sim.time_to_sleep)
+		|| (argc == 6 && parse_unsigned(argv[5], &sim.number_of_meals)))
+		return (ft_perror(INVALID_ARGS_ERR), 2);
+	if (pthread_mutex_init(&sim.write, NULL))
+		return (ft_perror(MUTEX_INIT_ERR), 3);
+	sim.philos = create_philos(sim);
+	if (!sim.philos)
+		return (pthread_mutex_destroy(&sim.write), 4);
 	return (0);
+}
+
+/*
+	whitespace characters: '\t', '\n', '\v', '\f', '\r', ' '
+					ascii: 9, 10, 11, 12, 13, 32
+*/
+static int	parse_unsigned(char *str, unsigned int *result)
+{
+	unsigned int	prev_res;
+
+	if (!str || !result)
+		return (1);
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+		str++;
+	if (*str < '0' || *str > '9')
+		return (2);
+	*result = (*str++ - '0');
+	while (*str >= '0' && *str <= '9')
+	{
+		prev_res = *result;
+		*result = *result * 10 + (*str++ - '0');
+		if (prev_res > *result)
+			return (3);
+	}
+	while (*str == ' ' || (*str >= '\t' && *str <= '\r'))
+		str++;
+	if (*str)
+		return (4);
+	return (0);
+}
+
+t_philo	*create_philos(t_simulation	sim)
+{
+	t_philo			*philos;
+	unsigned int	i;
+
+	philos = malloc(sizeof(t_philo) * sim.number_of_philo);
+	if (!sim.philos)
+		return (ft_perror(MALLOC_ERR), NULL);
+	i = 0;
+	while (i < sim.number_of_philo)
+	{
+		philos[i].id = i + 1;
+		philos[i].sim = sim;
+		if (pthread_mutex_init(&philos[i].fork, NULL))
+		{
+			while (--i)
+				pthread_mutex_destroy(&philos[i].fork);
+			return (free(philos), NULL);
+		}
+		i++;
+	}
+	return (philos);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,28 +6,45 @@
 /*   By: susajid <susajid@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 15:54:45 by susajid           #+#    #+#             */
-/*   Updated: 2024/01/22 12:00:41 by susajid          ###   ########.fr       */
+/*   Updated: 2024/01/24 16:32:07 by susajid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <stdlib.h>
+# include <unistd.h>
 # include <stdbool.h>
+# include <pthread.h>
+
+# define USAGE_ERR "usage: ./philo <number_of_philo> <time_to_die> <time_to_eat> \
+<time_to_sleep> [number_of_meals]"
+# define INVALID_ARGS_ERR "an invalid argument was entered"
+# define THREAD_CREATE_ERR "could not create thread"
+# define THREAD_JOIN_ERR "could not join thread"
+# define MALLOC_ERR "a memory allocation error occurred"
+# define MUTEX_INIT_ERR "could not initialize mutex"
 
 typedef struct s_simulation
 {
+	unsigned int	number_of_philo;
 	unsigned int	time_to_die;
 	unsigned int	time_to_eat;
 	unsigned int	time_to_sleep;
-	unsigned int	number_to_eat;
-	unsigned int	philo_died;
+	unsigned int	number_of_meals;
+	struct s_philo	*philos;
+	pthread_mutex_t	write;
+	bool			quit;
 }	t_simulation;
 
 typedef struct s_philo
 {
-	unsigned int	id;
-	t_simulation	sim;
+	unsigned int		id;
+	struct s_simulation	sim;
+	pthread_mutex_t		fork;
 }	t_philo;
+
+void	ft_perror(char *msg);
 
 #endif /* PHILO_H */
